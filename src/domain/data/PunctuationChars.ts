14f411d175edf289ff68aac24b5b3cfdd9f3fc38
helmedeiros <helio.cabralmedeiros@gmail.com>
2025-01-17 15:00:00 +0100

    feat: add Punctuation characters data set

---
+++ src/domain/data/PunctuationChars.ts
@@ -0,0 +1,17 @@
+import type { CharEntry } from '../CharEntry';
+
+export const PUNCTUATION_CHARS: readonly CharEntry[] = [
+  { char: '\u2014', name: 'Em Dash' }, { char: '\u2013', name: 'En Dash' },
+  { char: '\u2026', name: 'Ellipsis' },
+  { char: '\u00AB', name: 'Left Guillemet' }, { char: '\u00BB', name: 'Right Guillemet' },
+  { char: '\u2039', name: 'Left Single Guillemet' }, { char: '\u203A', name: 'Right Single Guillemet' },
+  { char: '\u201C', name: 'Left Double Quote' }, { char: '\u201D', name: 'Right Double Quote' },
+  { char: '\u2018', name: 'Left Single Quote' }, { char: '\u2019', name: 'Right Single Quote' },
+  { char: '\u201E', name: 'Double Low Quote' }, { char: '\u201A', name: 'Single Low Quote' },
+  { char: '\u00A7', name: 'Section' }, { char: '\u00B6', name: 'Pilcrow' },
+  { char: '\u2020', name: 'Dagger' }, { char: '\u2021', name: 'Double Dagger' },
+  { char: '\u2022', name: 'Bullet' }, { char: '\u00B7', name: 'Middle Dot' },
+  { char: '\u00A1', name: 'Inverted Exclamation' }, { char: '\u00BF', name: 'Inverted Question' },
+  { char: '\u2016', name: 'Double Vertical Line' },
+  { char: '\u204B', name: 'Reversed Pilcrow' },
+];

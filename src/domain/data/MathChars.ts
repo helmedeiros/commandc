49a3078d70e1c67de4089248111826f0c075fff9
helmedeiros <helio.cabralmedeiros@gmail.com>
2025-01-18 15:00:00 +0100

    feat: add Math characters data set

---
+++ src/domain/data/MathChars.ts
@@ -0,0 +1,21 @@
+import type { CharEntry } from '../CharEntry';
+
+export const MATH_CHARS: readonly CharEntry[] = [
+  { char: '\u00B1', name: 'Plus Minus' }, { char: '\u00D7', name: 'Multiply' },
+  { char: '\u00F7', name: 'Divide' }, { char: '\u2260', name: 'Not Equal' },
+  { char: '\u2264', name: 'Less or Equal' }, { char: '\u2265', name: 'Greater or Equal' },
+  { char: '\u221E', name: 'Infinity' }, { char: '\u2248', name: 'Almost Equal' },
+  { char: '\u2261', name: 'Identical' }, { char: '\u221A', name: 'Square Root' },
+  { char: '\u221B', name: 'Cube Root' }, { char: '\u03C0', name: 'Pi' },
+  { char: '\u2211', name: 'Summation' }, { char: '\u220F', name: 'Product' },
+  { char: '\u222B', name: 'Integral' }, { char: '\u2202', name: 'Partial Differential' },
+  { char: '\u0394', name: 'Delta' }, { char: '\u2207', name: 'Nabla' },
+  { char: '\u2200', name: 'For All' }, { char: '\u2203', name: 'There Exists' },
+  { char: '\u2205', name: 'Empty Set' }, { char: '\u2208', name: 'Element Of' },
+  { char: '\u2209', name: 'Not Element Of' }, { char: '\u2229', name: 'Intersection' },
+  { char: '\u222A', name: 'Union' }, { char: '\u2282', name: 'Subset' },
+  { char: '\u2283', name: 'Superset' }, { char: '\u2227', name: 'Logical And' },
+  { char: '\u2228', name: 'Logical Or' }, { char: '\u00AC', name: 'Not' },
+  { char: '\u2234', name: 'Therefore' }, { char: '\u2235', name: 'Because' },
+  { char: '\u00B2', name: 'Superscript 2' }, { char: '\u00B3', name: 'Superscript 3' },
+];

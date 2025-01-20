0f7e194e8dc7a26b724d9d085869520bce463650
helmedeiros <helio.cabralmedeiros@gmail.com>
2025-01-20 15:00:00 +0100

    feat: add Currency characters data set

---
+++ src/domain/data/CurrencyChars.ts
@@ -0,0 +1,14 @@
+import type { CharEntry } from '../CharEntry';
+
+export const CURRENCY_CHARS: readonly CharEntry[] = [
+  { char: '\u0024', name: 'Dollar' }, { char: '\u20AC', name: 'Euro' },
+  { char: '\u00A3', name: 'Pound' }, { char: '\u00A5', name: 'Yen' },
+  { char: '\u20B9', name: 'Indian Rupee' }, { char: '\u20BF', name: 'Bitcoin' },
+  { char: '\u00A2', name: 'Cent' }, { char: '\u20A9', name: 'Won' },
+  { char: '\u20BD', name: 'Ruble' }, { char: '\u20AB', name: 'Dong' },
+  { char: '\u20B1', name: 'Peso' }, { char: '\u20B4', name: 'Hryvnia' },
+  { char: '\u20A8', name: 'Rupee' }, { char: '\u20BA', name: 'Lira' },
+  { char: '\u20B5', name: 'Cedi' }, { char: '\u00A4', name: 'Currency' },
+  { char: '\u20A3', name: 'French Franc' }, { char: '\u20A6', name: 'Naira' },
+  { char: '\u20A7', name: 'Peseta' },
+];

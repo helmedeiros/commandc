bb40f5301a69cff1373ba3a3ed63002d63c839e4
helmedeiros <helio.cabralmedeiros@gmail.com>
2025-01-22 15:00:00 +0100

    feat: add Hieroglyphs characters data set

---
+++ src/domain/data/HieroglyphsChars.ts
@@ -0,0 +1,16 @@
+import type { CharEntry } from '../CharEntry';
+
+export const HIEROGLYPHS_CHARS: readonly CharEntry[] = [
+  { char: '\u{13000}', name: 'Egyptian A' }, { char: '\u{13001}', name: 'Egyptian B' },
+  { char: '\u{13002}', name: 'Egyptian C' }, { char: '\u{13003}', name: 'Egyptian D' },
+  { char: '\u{13004}', name: 'Egyptian E' }, { char: '\u{13005}', name: 'Egyptian F' },
+  { char: '\u{13006}', name: 'Egyptian G' }, { char: '\u{13007}', name: 'Egyptian H' },
+  { char: '\u{13008}', name: 'Egyptian I' }, { char: '\u{13009}', name: 'Egyptian J' },
+  { char: '\u{1300A}', name: 'Egyptian K' }, { char: '\u{1300B}', name: 'Egyptian L' },
+  { char: '\u{1300C}', name: 'Egyptian M' }, { char: '\u{1300D}', name: 'Egyptian N' },
+  { char: '\u{1300E}', name: 'Egyptian O' }, { char: '\u{1300F}', name: 'Egyptian P' },
+  { char: '\u{13010}', name: 'Egyptian Q' }, { char: '\u{13011}', name: 'Egyptian R' },
+  { char: '\u{13012}', name: 'Egyptian S' }, { char: '\u{13013}', name: 'Egyptian T' },
+  { char: '\u{13014}', name: 'Egyptian U' }, { char: '\u{13015}', name: 'Egyptian V' },
+  { char: '\u{13016}', name: 'Egyptian W' }, { char: '\u{13017}', name: 'Egyptian X' },
+];

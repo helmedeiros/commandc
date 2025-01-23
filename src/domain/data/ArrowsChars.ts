60f456711527a8585ac2cc1dd9fcfd9b8bf2b144
helmedeiros <helio.cabralmedeiros@gmail.com>
2025-01-23 15:00:00 +0100

    feat: add Arrows characters data set

---
+++ src/domain/data/ArrowsChars.ts
@@ -0,0 +1,18 @@
+import type { CharEntry } from '../CharEntry';
+
+export const ARROWS_CHARS: readonly CharEntry[] = [
+  { char: '\u2190', name: 'Left' }, { char: '\u2191', name: 'Up' },
+  { char: '\u2192', name: 'Right' }, { char: '\u2193', name: 'Down' },
+  { char: '\u2194', name: 'Left Right' }, { char: '\u2195', name: 'Up Down' },
+  { char: '\u2196', name: 'Upper Left' }, { char: '\u2197', name: 'Upper Right' },
+  { char: '\u2198', name: 'Lower Right' }, { char: '\u2199', name: 'Lower Left' },
+  { char: '\u21D0', name: 'Double Left' }, { char: '\u21D1', name: 'Double Up' },
+  { char: '\u21D2', name: 'Double Right' }, { char: '\u21D3', name: 'Double Down' },
+  { char: '\u21D4', name: 'Double Left Right' }, { char: '\u21D5', name: 'Double Up Down' },
+  { char: '\u21A9', name: 'Left Hook' }, { char: '\u21AA', name: 'Right Hook' },
+  { char: '\u21B0', name: 'Up Left' }, { char: '\u21B1', name: 'Up Right' },
+  { char: '\u21BA', name: 'Anticlockwise' }, { char: '\u21BB', name: 'Clockwise' },
+  { char: '\u21C4', name: 'Right Over Left' }, { char: '\u21C6', name: 'Left Over Right' },
+  { char: '\u27A1', name: 'Black Right' }, { char: '\u2B05', name: 'Black Left' },
+  { char: '\u2B06', name: 'Black Up' }, { char: '\u2B07', name: 'Black Down' },
+];
